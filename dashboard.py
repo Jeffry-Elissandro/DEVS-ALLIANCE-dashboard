53c1e89bcd3732c0f179723400fea91ecafb483f
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-01-19 03:31:31 -0600

    Update V1.14
    Change the line

--- dashboard.py
+++ dashboard.py
@@ -434,8 +434,7 @@ fig = px.bar(
         "Consistencia": True,
         "Score": False
     },
-    title="Rendimiento de los miembros - Semana de Gremios 12/18 Enero 2026",
-    title="VER EN HORIZONTAL!✅"
+    title="Rendimiento de los miembros - Semana de Gremios 12/18 Enero 2026                                    VER EN HORIZONTAL ✅"
 )
 
 fig.update_traces(texttemplate="%{text}%", textposition="outside")
