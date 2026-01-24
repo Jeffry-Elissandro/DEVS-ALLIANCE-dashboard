c328f2a4e63bf351bded1c7f7980e644606dff84
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-01-24 16:33:18 -0600

    Update V1.39
    Using \n test

--- dashboard.py
+++ dashboard.py
@@ -582,9 +582,9 @@ st.markdown(
     font-size: 16px;
     line-height: 1.6;
     ">
-    ¡Saludos Gente!
-    ¿Tienes problemas para hacer más daño al Jefe de Inmortales?,
-    No te preocupes... te mostraré el equipo ideal para que logres,
+    ¡Saludos Gente!\n
+    ¿Tienes problemas para hacer más daño al Jefe de Inmortales?\n
+    No te preocupes... te mostraré el equipo ideal para que logres\n
     hacer más daño y puedas superarte esta temporada de Gremios!.
     </p>
 
