d9b12294e0876735b0104f235951096c44e20151
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-01-27 13:45:21 -0600

    Update V1.99
    Agree score guild.

--- dashboard.py
+++ dashboard.py
@@ -488,7 +488,7 @@ components.html(
 # PROGRESO HACIA DIAMANTE
 # ============================
 
-puntaje_actual = 4000   # Oro actual
+puntaje_actual = 5280   # Oro actual
 puntaje_meta = 16000     # Diamante
 
 porcentaje = int((puntaje_actual / puntaje_meta) * 100)
