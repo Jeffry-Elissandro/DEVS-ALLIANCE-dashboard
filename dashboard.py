2fe6df92b5d6d182284fdc876abb4094b26ae085
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-02-04 19:13:37 -0600

    Update V3.15
    Testing the hitbox.

--- dashboard.py
+++ dashboard.py
@@ -447,9 +447,8 @@ if st.session_state.mostrar_nota:
 #Para Promocionar el nuevo JEFE MS.FORTUNE
 
 
-import streamlit as st
-import base64
 import streamlit.components.v1 as components
+import base64
 
 def img_base64(path):
     with open(path, "rb") as f:
@@ -460,21 +459,19 @@ misterio_img = img_base64("Critty_Kitty.png")
 components.html(
     f"""
     <div style="
-      max-width: 1000px;
+      max-width: 90%;
       margin: 40px auto;
       padding: 18px;
       background: linear-gradient(180deg, #0f0f0f, #1c1c1c);
       border-radius: 22px;
       border: 2px solid rgba(180,180,180,0.55);
-      box-shadow:
-        0 0 25px rgba(120,120,120,0.45),
-        inset 0 0 18px rgba(80,80,80,0.35);
+      box-shadow: 0 0 25px rgba(120,120,120,0.45),
+                  inset 0 0 18px rgba(80,80,80,0.35);
       text-align: center;
     ">
 
-      <!-- Título -->
       <h2 style="
-        font-size: 32px;
+        font-size: 2.5vw;
         color: #ffffff;
         text-shadow: 0 0 12px rgba(255,255,255,0.9),
                      0 0 24px rgba(200,200,200,0.7);
@@ -483,20 +480,18 @@ components.html(
         MUY PRONTO
       </h2>
 
-      <!-- Imagen -->
       <img src="data:image/png;base64,{misterio_img}" style="
         width: 100%;
         height: auto;
         border-radius: 12px;
         box-shadow: 0 0 25px rgba(255,255,255,0.15);
-        display: block;
-        margin: 0 auto;
       " />
 
     </div>
     """,
-    height=None  # deja que Streamlit calcule la altura automáticamente
+    height=900  # reserva suficiente espacio vertical
 )
+
 
 
 
