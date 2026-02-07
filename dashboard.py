82bce7ef47a07a1e8aeff7c68481cec6845fc20d
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-02-06 21:32:39 -0600

    Upgrade V3.25
    Upgrade the score guidl 11500 to 12415!

--- dashboard.py
+++ dashboard.py
@@ -587,7 +587,7 @@ components.html(
 # PROGRESO HACIA DIAMANTE
 # ============================
 
-puntaje_actual = 11500   # Oro actual
+puntaje_actual = 12415   # Oro actual
 puntaje_meta = 16000     # Diamante
 
 porcentaje = int((puntaje_actual / puntaje_meta) * 100)
