a00c020e9d9301c7e86dd3da5d1c318ad5e10e38
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-02-08 17:30:27 -0600

    Update V3.42
    Change the gif of Rank Diamond!

--- dashboard.py
+++ dashboard.py
@@ -747,7 +747,7 @@ diamante_img = img_to_base64("Gremio_Diamante_Logotipo.png")
 
 
 
-file_path = "Skull_characteres.gif"
+file_path = "test_gif.gif" #Para el gif
 with open(file_path, "rb") as f:
     data = f.read()
     encoded = base64.b64encode(data).decode("utf-8")
