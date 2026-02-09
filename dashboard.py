afe82a74cc6143af020dee5f39e681a90ee14e22
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-02-08 18:22:16 -0600

    Update V3.51
    FINALY 16000!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- dashboard.py
+++ dashboard.py
@@ -663,7 +663,7 @@ components.html(
 # PROGRESO HACIA DIAMANTE
 # ============================
 
-puntaje_actual = 15850   # Oro actual
+puntaje_actual = 16000   # Oro actual
 puntaje_meta = 16000     # Diamante
 
 porcentaje = int((puntaje_actual / puntaje_meta) * 100)
