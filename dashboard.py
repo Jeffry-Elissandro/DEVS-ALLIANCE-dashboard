189a7195b96872283145fd42bd77435f95201c7f
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-02-10 21:06:38 -0600

    Update V3.60
    Fixing bugs again.

--- dashboard.py
+++ dashboard.py
@@ -1348,7 +1348,7 @@ components.html(
                 <iframe
                     width="100%"
                     height="500"
-                    src="https://youtu.be/XwXHEG6iJbE"
+                    src="https://www.youtube.com/embed/XwXHEG6iJbE"
                     title="YouTube video player"
                     frameborder="0"
                     allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
