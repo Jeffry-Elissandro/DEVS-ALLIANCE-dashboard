e5fc2746066fea0fd19aef669e9dce3f79fff9c3
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-03-07 06:54:24 -0600

    Update V7.5
    Change Video at chanel at for web.

--- dashboard.py
+++ dashboard.py
@@ -3175,7 +3175,7 @@ st.markdown("""
       Aquí te comparto mi más reciente novedad en el canal.
   </p>
   <div class="ultimo-video-box">
-      <iframe src="https://www.youtube.com/embed/3UokxHsoiAs" 
+      <iframe src="https://www.youtube.com/embed/GChtzMhJSnI" 
               title="YouTube video player" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
               allowfullscreen>
