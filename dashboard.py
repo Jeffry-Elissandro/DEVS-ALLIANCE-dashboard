e8acea0a66da6bfebd39fe9228dd312b3aae5f1c
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-04-12 15:11:21 -0600

    Update V9.53
    Fix word

--- dashboard.py
+++ dashboard.py
@@ -838,7 +838,7 @@ diamante_img = img_to_base64("Gremio_Diamante_Logotipo.png")
 
 
 
-file_path = "Test_gif.gif" #Para el gif
+file_path = "test_gif.gif" #Para el gif
 with open(file_path, "rb") as f:
     data = f.read()
     encoded = base64.b64encode(data).decode("utf-8")
