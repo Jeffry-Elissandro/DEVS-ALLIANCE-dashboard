17a916c74c43f8516bcdcc5b6a33287f8f73f47c
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-04-24 18:26:23 -0600

    Update V9.73
    Improve the score guild to 13010!!!

--- dashboard.py
+++ dashboard.py
@@ -555,7 +555,7 @@ st.divider()
 # PROGRESO HACIA DIAMANTE
 # ============================
 
-puntaje_actual = 11015   # Oro actual
+puntaje_actual = 13010   # Oro actual
 puntaje_meta = 16000     # Diamante
 
 porcentaje = int((puntaje_actual / puntaje_meta) * 100)
