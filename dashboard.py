9ce8d6c290f640fca23650bbe5beaf4c5a63d186
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-06-14 23:39:09 -0600

    Update V10.38
    Change my last Video to "OHAYO"!!!

--- dashboard.py
+++ dashboard.py
@@ -3528,7 +3528,7 @@ st.markdown("""
       Aquí te comparto mi más reciente novedad en el canal.
   </p>
   <div class="ultimo-video-box">
-      <iframe src="https://www.youtube.com/embed/qrRS86QDUJ0" 
+      <iframe src="https://www.youtube.com/embed/6DKUOJPXbbA" 
               title="YouTube video player" 
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
               allowfullscreen>
