f44a85561b79b0340868b16c43e41f0077a3a0bb
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-06-28 16:46:11 -0600

    UPDATE V10.44!!
    STRUCTURE FOR DEV'S ALLIANCE 2.0

---
+++ assets/test.py
@@ -0,0 +1,25 @@
+#1 - Noticias Iniciales y Recientes.
+#2 - Video y Guía de Gremios + Nuestro Mejor Récord (funciona como sección de tributo al trabajo en equipo).
+#3 - Regla para estar en el Gremio y Condiciones de Participación.
+#4 - Progreso Actual a Diamante (se actualiza constantemente para ver el progreso casi en tiempo real).
+#5 - Resumen de temporada y objetivos (Resume los resultados como TOP mejor y los objetivos semanales).
+#6 - Tabla y Gráfica de estadísticas de la temporada de los miembros y Sección que explica cómo se evalua el rendimiento, además de invitar a solucionar errores si se comente alguno.
+#7 - Equipo para el de Jefe de la temporada (videos + cartas.png).
+#8 - Mensaje de agradecimiento, Miembros Actuales e Incorporaciones.
+#9 - Fan-arts y Meme del Día.
+#10 - Comentarios y Opiniones + Botón con Contraseña para limpiar Todo el Historial de comentarios visibles.
+#11 - Contacto con el Administrador + mi Última Novedad.
+#12 - Musica del Gremio, Soundtrack y Recursos.
+
+#1 - home.py
+#2 - legacy.py
+#3 - rules.py
+#4 - progress.py
+#5 - season.py
+#6 - statistics.py
+#7 - strategy.py
+#8 - community.py
+#9 - gallery.py
+#10 - comments.py
+#11 - contact.py
+#12 - media.py
