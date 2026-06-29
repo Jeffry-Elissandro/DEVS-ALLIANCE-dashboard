0a5b004fe97ac044e39e81999a518e716a9cf331
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-06-29 04:24:58 -0600

    Update V10.58
    testing components/hero/hero_card.py

--- components/hero/hero_card.py
+++ components/hero/hero_card.py
@@ -1,37 +1,19 @@
-"""
-components/hero/hero_card.py
-------------------------------------
-Contenedor principal de la Hero Section.
-"""
-
 import streamlit as st
 
 
 def open_hero_card() -> None:
-    """
-    Abre la tarjeta principal.
-    """
-
     st.markdown(
         """
-        <main class="landing">
-
-            <section class="hero-card">
+        <div class="hero-card">
         """,
         unsafe_allow_html=True,
     )
 
 
 def close_hero_card() -> None:
-    """
-    Cierra la tarjeta principal.
-    """
-
     st.markdown(
         """
-            </section>
-
-        </main>
+        </div>
         """,
         unsafe_allow_html=True,
     )
