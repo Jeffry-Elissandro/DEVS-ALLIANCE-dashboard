f9b6f0c676ca59916b54a92f25c4edeb2097a1be
Jeffry-Elissandro <jeffryelissandrocarmonasantos@gmail.com>
2026-06-29 04:42:42 -0600

    Update V10.61
    testing this...

--- components/hero/hero_title.py
+++ components/hero/hero_title.py
@@ -1,21 +1,10 @@
-"""
-components/hero/hero_title.py
-"""
-
-
 def render_title() -> str:
-    """
-    Devuelve el HTML del título principal.
-    """
 
-    return """
-    <div class="title-container">
+    return """<div class="title-container">
+<h1>DEV'S ALLIANCE</h1>
 
-        <h1>DEV'S ALLIANCE</h1>
+<p class="subtitle">
+Comunidad Oficial del Gremio
+</p>
 
-        <p class="subtitle">
-            Comunidad Oficial del Gremio
-        </p>
-
-    </div>
-    """
+</div>"""
